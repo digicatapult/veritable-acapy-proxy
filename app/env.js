d2c11e4f3e2e6ae9d18c7d7172d5078c5abe4ae6
Darryl Morton <darryl.morton@digicatapult.org.uk>
2022-01-05 12:08:29 +0000

    removed dep, appUtil and corresponding envs for polkadot

--- app/env.js
+++ app/env.js
@@ -14,13 +14,9 @@ const vars = envalid.cleanEnv(
     SERVICE_TYPE: envalid.str({ default: 'vitalam-service-template'.toUpperCase().replace(/-/g, '_') }),
     PORT: envalid.port({ default: 80, devDefault: 3000 }),
     API_HOST: envalid.host({ devDefault: 'localhost' }),
-    API_RPC_PORT: envalid.port({ default: 9944 }),
     API_VERSION: envalid.str({ default: version }),
     API_MAJOR_VERSION: envalid.str({ default: 'v1' }),
     LOG_LEVEL: envalid.str({ default: 'info', devDefault: 'debug' }),
-    USER_URI: envalid.str({ devDefault: '//Alice' }),
-    METADATA_KEY_LENGTH: envalid.num({ default: 32 }),
-    METADATA_VALUE_LITERAL_LENGTH: envalid.num({ default: 32 }),
   },
   {
     strict: true,
