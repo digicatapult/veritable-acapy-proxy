8436f15a5a213d97e3a00a912c2e024e47c6d471
Matthew Dean <matthew.dean@digicatapult.org.uk>
2022-03-17 15:21:47 +0000

    Unit tests for spec transform

--- test/unit/transformApiDoc.test.js
+++ test/unit/transformApiDoc.test.js
@@ -1,1 +1,21 @@
-// TODO: unit tests for transform
+const { describe, it } = require('mocha')
+const { expect } = require('chai')
+
+const transform = require('../../app/util/transformApiDoc')
+const input = require('./data/input.json')
+const expectation1 = require('./data/expectation-1.json')
+const expectation2 = require('./data/expectation-2.json')
+
+describe('transformApiDoc', function () {
+  it('should transform input -> expectation (case 1)', function () {
+    expect(transform(input, { title: 'TITLE-1', version: '1.0.0', pathPrefix: '/prefix-1' })).to.deep.equal(
+      expectation1
+    )
+  })
+
+  it('should transform input -> expectation (case 2)', function () {
+    expect(transform(input, { title: 'TITLE-2', version: '2.0.0', pathPrefix: '/prefix-2' })).to.deep.equal(
+      expectation2
+    )
+  })
+})
