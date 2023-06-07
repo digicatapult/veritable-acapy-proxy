64024a86d19b9b0feb90c6a75c6a1ef0707b9a32
Paulius Michelevicius <paulius.michelevicius@gmail.com>
2023-06-07 15:23:30 +0100

    VER-3: version check script update.

--- scripts/check-version.sh
+++ scripts/check-version.sh
@@ -46,22 +46,22 @@ function check_version_greater () {
   fi
 }
 
-# Get published git tags that match semver regex with a "v" prefix then remove the "v" character
-PUBLISHED_VERSIONS=$(git tag | grep "^v[0-9]\+\.[0-9]\+\.[0-9]\+\(\-[a-zA-Z-]\+\(\.[0-9]\+\)*\)\{0,1\}$" | sed 's/^v\(.*\)$/\1/')
+# Get published versions from the npm registry
+PUBLISHED_VERSIONS=$(curl -s https://registry.npmjs.org/@digicatapult/ui-component-library |jq -r ' .versions[].version')
 # Get the current version from package.json
 CURRENT_VERSION=$(yq eval '.version' ./package.json)
 
 if check_version_greater "$CURRENT_VERSION" "$PUBLISHED_VERSIONS"; then
-  echo "##[set-output name=VERSION;]v$CURRENT_VERSION"
-  echo "##[set-output name=BUILD_DATE;]$(date -u +'%Y-%m-%dT%H:%M:%SZ')"
-  echo "##[set-output name=IS_NEW_VERSION;]true"
+  echo "VERSION=v$CURRENT_VERSION" >> $GITHUB_OUTPUT
+  echo "BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')" >> $GITHUB_OUTPUT
+  echo "IS_NEW_VERSION=true" >> $GITHUB_OUTPUT
   if [[ $CURRENT_VERSION =~ [-] ]]; then
-    echo "##[set-output name=IS_PRERELEASE;]true"
-    echo "##[set-output name=NPM_RELEASE_TAG;]next"
+    echo "IS_PRERELEASE=true" >> $GITHUB_OUTPUT
+    echo "NPM_RELEASE_TAG=next" >> $GITHUB_OUTPUT
   else
-    echo "##[set-output name=IS_PRERELEASE;]false"
-    echo "##[set-output name=NPM_RELEASE_TAG;]latest"
+    echo "IS_PRERELEASE=false" >> $GITHUB_OUTPUT
+    echo "NPM_RELEASE_TAG=latest" >> $GITHUB_OUTPUT
   fi
 else
-  echo "##[set-output name=IS_NEW_VERSION;]false"
+  echo "IS_NEW_VERSION=false" >> $GITHUB_OUTPUT
 fi
