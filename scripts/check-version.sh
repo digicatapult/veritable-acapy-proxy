573e4dc843bf71dac34e7dc35c54a36b600df307
Paulius <paulius.michelevicius@gmail.com>
2023-06-12 06:34:29 +0100

    VER-3: remove refs to helm.

--- scripts/check-version.sh
+++ scripts/check-version.sh
@@ -6,14 +6,8 @@ set -e
 function check_versions_consistent () {
   local PACKAGE_VERSION=$(yq eval -o y '.version' ./package.json)
   local PACKAGE_LOCK_VERSION=$(yq eval -o y '.version' ./package-lock.json)
-  local HELM_VALUES_TAG_VERSION=$(yq eval -o y '.image.tag' ./helm/veritable-acapy-proxy/values.yaml)
-  local HELM_CHART_VERSION=$(yq eval -o y '.version' ./helm/veritable-acapy-proxy/Chart.yaml)
-  local HELM_CHART_APP_VERSION=$(yq eval -o y '.appVersion' ./helm/veritable-acapy-proxy/Chart.yaml)
 
-  if [ "$PACKAGE_VERSION" != "$PACKAGE_LOCK_VERSION" ] ||
-    [ "v$PACKAGE_VERSION" != "$HELM_VALUES_TAG_VERSION" ] ||
-    [ "$PACKAGE_VERSION" != "$HELM_CHART_VERSION" ] ||
-    [ "$PACKAGE_VERSION" != "$HELM_CHART_APP_VERSION" ]; then
+  if [ "$PACKAGE_VERSION" != "$PACKAGE_LOCK_VERSION" ]; then
       echo "Inconsistent versions detected"
       echo "PACKAGE_VERSION: $PACKAGE_VERSION"
       echo "PACKAGE_LOCK_VERSION: $PACKAGE_LOCK_VERSION"
